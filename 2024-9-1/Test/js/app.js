8096e59af93056f1f888d2174fbe2ed5a433fb6b
Yatwohee <jisanduo@outlook.com>
2024-09-05 20:52:26 +0800

    更新了2024年9月1日开发文档

--- 2024-9-1/Test/js/app.js
+++ 2024-9-1/Test/js/app.js
@@ -1,4 +1,10 @@
-function SettingText(){
-	var Div = document.getElementById("Texts");
-	Div = "ds";
+/*
+	FileName : app.js
+	By : yzx
+	MakeTime : 2024年9月2日 上午 08:58:52
+	Copyright (C) 2024-2025 openGames 
+*/
+function SettingText() {
+	//在div标签内插入文本
+	var Div = document.getElementById("Texts").innerHTML = "ds";
 }
