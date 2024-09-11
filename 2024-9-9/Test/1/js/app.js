65cf2a365d2524bb8d51a2ee947756eaa21113b8
Yatwohee <jisanduo@outlook.com>
2024-09-11 21:12:19 +0800

    更新了2024年9月9日的开发文档

---
+++ 2024-9-9/Test/1/js/app.js
@@ -0,0 +1,10 @@
+/* 
+	FileName : app.js
+	By : yzx
+	MakeTime : 2024年9月9日 上午 8:58:52
+	Copyright (C) 2024-2025 openGames 
+*/
+function SettingText() {
+	var Div = document.getElementById("ThisText").innerText = "昨天在地铁站，看到一个爸爸抱着一个大号的哆啦a梦玩偶，儿子抱着一个小号的，儿子眼馋的看着爸爸的那个，爸爸说：乖！别吵我，这个可是给你妈妈的。。顿时觉得好温馨。。。";
+}
+SettingText();
